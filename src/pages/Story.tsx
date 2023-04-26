418b289946cecd0d141bafc2eeac3f001be1f64b
kristjansig1983 <kristjansig1983@gmail.com>
2023-04-26 09:50:06 +0000

    Working on history
    Still working on history. Not quite working as I would like.

--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,68 +1,84 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-function Story() {
-  return (
-    <MainPage>
-      <Header />
-      <History>
-        <HistoryText>
-          The Vintage Caravan are a rock band from Álftanes, Iceland. The band
-          was formed in 2006 by Óskar Logi Ágústsson and Guðjón Reynisson before
-          bassist Alexander Örn Númason joined after the first studio album. As
-          of 2021, the band have released five studio albums and are currently
-          signed with Napalm Records.
-        </HistoryText>
-        <HistoryText>
-          In 2006, 12 year-old school friends Óskar Logi Ágústsson (guitar and
-          vocals) and Guðjón Reynisson (drums) formed the band. Serious work
-          only began in 2009 and within two years the band self-published their
-          eponymous first album. In early 2012, the band signed a deal with
-          Icelandic record label Sena ahead of work for their second album.
-          Alexander Örn Númason joined shortly after on bass. A second album
-          Voyage was released in 2012 initially by Sena. The band signed to
-          Nuclear Blast shortly after the release of Voyage. This led to a
-          Expand Your Mind music video produced and released by the German
-          record label. The short film shows the psychedelic 'trips' the three
-          members experience. As of July 2015, the video reached 400,000 hits on
-          YouTube becoming the most viewed online song by the band. Nuclear
-          Blast re-released Voyage to a much wider audience.[2] 2014 saw the
-          band playing at festivals such as Roadburn Festival, Wacken Open Air
-          and Hard Rock Hell.[citation needed] In April 2015, the band announced
-          on Facebook that drummer Guðjón Reynisson had left the band for
-          personal reasons however still remained good friends with the
-          remaining band members. In the same post, Stefán Ari was introduced to
-          fans as the new drummer of the band. On 15 May 2015, Arrival was
-          released with Nuclear Blast as the band's third studio album. This was
-          the first record since the recent line-up change.[citation needed] The
-          band's four studio album Gateways was released in 2018.[3] Two years
-          later the band signed with Napalm Records ahead of the release of the
-          fifth album Monuments. Much like previous albums, Monuments received
-          mostly positive reviews, with Loudersound in particular finishing
-          their 4 out of 5 star review with the sentence 'The Vintage Caravan
-          just get better and better.'[4]
-        </HistoryText>
-      </History>
+interface ParagraphWithImageProps {
+  src: string
+  paragraphText: string
+  imageOnRight: boolean
+}
 
-      <Footer />
-    </MainPage>
+const ParagraphContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`
+
+const Image = styled.img<{ imageOnRight: boolean }>`
+  height: 150px;
+  margin-right: ${(props) => (props.imageOnRight ? '20px' : '0')};
+  margin-left: ${(props) => (props.imageOnRight ? '0' : '20px')};
+
+  ${(props) =>
+    props.imageOnRight &&
+    css`
+      order: 2;
+    `}
+
+  ${(props) =>
+    !props.imageOnRight &&
+    css`
+      order: 1;
+    `}
+`
+
+const Paragraph = styled.p``
+
+const ParagraphWithImage: React.FC<ParagraphWithImageProps> = ({
+  src,
+  paragraphText,
+  imageOnRight,
+}) => {
+  return (
+    <ParagraphContainer>
+      {imageOnRight ? (
+        <>
+          <Paragraph>{paragraphText}</Paragraph>
+          <Image src={src} imageOnRight={imageOnRight} />
+        </>
+      ) : (
+        <>
+          <Image src={src} imageOnRight={imageOnRight} />
+          <Paragraph>{paragraphText}</Paragraph>
+        </>
+      )}
+    </ParagraphContainer>
   )
 }
 
-const MainPage = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-`
-const History = styled.div`
-  display: flex;
-  justify-content: center;
-`
-const HistoryText = styled.p`
-  display: flex;
-`
+function Story() {
+  return (
+    <div>
+      <Header />
+      <ParagraphWithImage
+        src='https://i2.wp.com/secretsolstice.is/wp-content/uploads/2019/02/vintage-caravan-main-1.jpg?fit=1200%2C750&ssl=1'
+        paragraphText='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+        imageOnRight={true}
+      />
+      <ParagraphWithImage
+        src='https://i2.wp.com/secretsolstice.is/wp-content/uploads/2019/02/vintage-caravan-main-1.jpg?fit=1200%2C750&ssl=1'
+        paragraphText='Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        imageOnRight={false}
+      />
+      <ParagraphWithImage
+        src='https://i2.wp.com/secretsolstice.is/wp-content/uploads/2019/02/vintage-caravan-main-1.jpg?fit=1200%2C750&ssl=1'
+        paragraphText='Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        imageOnRight={true}
+      />
+      <Footer />
+    </div>
+  )
+}
 
 export default Story
